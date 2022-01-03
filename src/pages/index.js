781f5be49b8c252ab7b42a4bb620b078a0efc4c8
chair28980 <vrycmfy@protonmail.com>
2022-01-03 00:57:59 -0500

    update desc

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="How to play the energyg game.">
       <HomepageHeader />
       <main>
         {/* <HomepageFeatures /> */}

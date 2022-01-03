da3691fe16f30235c51229109284a6aa50a4e255
chair28980 <vrycmfy@protonmail.com>
2022-01-02 22:58:45 -0500

    update name

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/aura-getting-started/login">
-            Tutorial
+            to="/docs/why-aura">
+            Go to docs
           </Link>
         </div>
       </div>
@@ -29,7 +29,7 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>

d5fcfb6bd556fcfa5880967aae269eca110642e6
chair28980 <vrycmfy@protonmail.com>
2022-01-02 22:52:06 -0500

    implement dark mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,6 +42,10 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: true,
+      },
       navbar: {
         title: 'AURA Instructions',
         logo: {

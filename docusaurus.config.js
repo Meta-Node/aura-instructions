7891e7ee49f9e16192e8a1ff7b6922a4fddd7536
chair28980 <vrycmfy@protonmail.com>
2022-01-02 23:20:41 -0500

    disrespect color scheme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,6 +45,7 @@ const config = {
       colorMode: {
         defaultMode: 'dark',
         disableSwitch: true,
+        respectPrefersColorScheme: false,
       },
       navbar: {
         title: 'AURA Docs',
